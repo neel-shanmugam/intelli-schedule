4cabd3c57d0abfc3595b7011cb9b3c3009d8a2a4
neel-shanmugam <neel.shanmugam@gmail.com>
2023-06-18 17:13:26 -0500

    initial UI (not fully working)

---
+++ extension/scripts/content.js
@@ -0,0 +1,25 @@
+// HTML elements
+const taskInput = document.getElementById("task-input");
+const addButton = document.getElementById("add-button");
+const taskList = document.getElementById("task-list");
+
+// Event listener for the "Add" button
+addButton.addEventListener("click", addTask);
+
+// Event listener for the "Enter" key press
+taskInput.addEventListener("keypress", function (event) {
+  if (event.keyCode === 13) {
+    addTask();
+  }
+});
+
+// Function to add a task
+function addTask() {
+  const task = taskInput.value;
+  if (task.trim() !== "") {
+    const listItem = document.createElement("li");
+    listItem.textContent = task;
+    taskList.appendChild(listItem);
+    taskInput.value = "";
+  }
+}
